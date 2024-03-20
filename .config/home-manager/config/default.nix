95f7161ce1c08c395133f530f543e89b6c24ff18
joaopedroaat <joaopedroaat@outlook.com>
2024-03-20 12:50:01 -0300

    Move home to separated file

--- .config/home-manager/config/default.nix
+++ .config/home-manager/config/default.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: {
+{...}: {
   imports = [
     ./nvim
     ./git.nix
@@ -7,39 +7,6 @@
     ./ui
     ./tmux.nix
     ./ssh.nix
+    ./home.nix
   ];
-
-  home.username = "joaopedroaat";
-  home.homeDirectory = "/home/joaopedroaat";
-
-  home.stateVersion = "23.11"; # Do not change
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  home.packages = [
-    # Clipboard
-    pkgs.wl-clipboard
-    # Telescope dependencies
-    pkgs.fd
-    pkgs.ripgrep
-    # Spotify
-    pkgs.spotify
-    # Formatters
-    pkgs.nixpkgs-fmt
-    pkgs.prettierd
-    pkgs.yamlfmt
-    pkgs.alejandra
-    # Lazygit
-    pkgs.lazygit
-  ];
-
-  home.file = {
-  };
-
-  home.sessionVariables = {
-    EDITOR = "nvim";
-  };
-
-  programs.home-manager.enable = true;
 }
