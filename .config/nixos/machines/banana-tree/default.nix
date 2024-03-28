2d905bf386745ddb2c9cb811eb199575e4433f74
joaopedroaat <joaopedroaat@outlook.com>
2024-03-27 21:20:09 -0300

    Add gpu support

--- .config/nixos/machines/banana-tree/default.nix
+++ .config/nixos/machines/banana-tree/default.nix
@@ -1,3 +1,13 @@
-{...}: {
+{pkgs, ...}: {
   networking.hostName = "banana-tree";
+
+  # GPU
+  boot.initrd.kernelModules = ["amdgpu"];
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      rocmPackages.clr.icd
+      amdvlk
+    ];
+  };
 }
