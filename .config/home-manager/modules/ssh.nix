e97ec97f033b058642b37b729740d40df8e69585
joaopedroaat <joaopedroaat@outlook.com>
2024-04-19 17:55:30 -0300

    Change 1password / ssh interaction

--- .config/home-manager/modules/ssh.nix
+++ .config/home-manager/modules/ssh.nix
@@ -2,14 +2,16 @@
   lib,
   config,
   ...
-}: {
+}: let
+  onePassPath = "~/.1password/agent.sock";
+in {
   options.ssh.enable = lib.mkEnableOption "ssh";
   config = lib.mkIf config.ssh.enable {
     programs.ssh = {
       enable = true;
       extraConfig = ''
         Host *
-         IdentityAgent ~/.1password/agent.sock
+            IdentityAgent ${onePassPath}
       '';
     };
   };
