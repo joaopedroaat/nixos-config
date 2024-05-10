66b9e5d1be84c30cbd399a6ef3409bdb9a25b3c7
joaopedroaat <joaopedroaat@outlook.com>
2024-05-10 17:47:10 -0300

    Add scanner drivers

--- .config/nixos/modules/office/scanner.nix
+++ .config/nixos/modules/office/scanner.nix
@@ -1,10 +1,25 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }: {
   options.scanner.enable = lib.mkEnableOption "Scanner";
   config = lib.mkIf config.scanner.enable {
-    hardware.sane.enable = true; # enables support for SANE scanners
+    hardware.sane = {
+      enable = true;
+      extraBackends = with pkgs; [
+        # Driverless Apple AirScan and Microsoft WSD
+        sane-airscan
+        # Epson
+        epkowa
+        utsushi
+        # HP
+        hplipWithPlugin
+      ];
+    };
+
+    # Epson
+    services.udev.packages = with pkgs; [utsushi];
   };
 }
