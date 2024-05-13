28f887597cf119156cd36e8afa987bc64ebb1aa8
joaopedroaat <joaopedroaat@outlook.com>
2024-05-13 00:09:44 -0300

    Create .desktop for lf to open with firefox

--- .config/home-manager/common-configs.nix
+++ .config/home-manager/common-configs.nix
@@ -11,9 +11,35 @@
     GTK_THEME = config.gtk.theme.name;
   };
 
-  xdg.enable = true;
-  xdg.userDirs.enable = true;
-  xdg.userDirs.createDirectories = true;
+  xdg = {
+    enable = true;
+    userDirs = {
+      enable = true;
+      createDirectories = true;
+    };
+    desktopEntries = {
+      lf = {
+        type = "Application";
+        name = "lf";
+        comment = "Launches the lf file manager";
+        icon = "utilities-terminal";
+        terminal = false;
+        exec = "kitty -e lf";
+        categories = ["ConsoleOnly" "System" "FileTools" "FileManager"];
+        mimeType = ["inode/directory"];
+        settings = {
+          Keywords = "File;Manager;Browser;Explorer;Launcher";
+        };
+      };
+    };
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "inode/directory" = ["lf.desktop"];
+        "application/pdf" = ["firefox.desktop"];
+      };
+    };
+  };
 
   # Unfree packages
   nixpkgs.config.allowUnfreePredicate = pkg:
