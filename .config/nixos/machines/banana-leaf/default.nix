3b8567aed91e73b675a9c0c46faa4e4f22df96f7
joaopedroaat <Joao Pedro>
2024-05-15 00:23:10 -0300

    Add intel opengl

--- .config/nixos/machines/banana-leaf/default.nix
+++ .config/nixos/machines/banana-leaf/default.nix
@@ -1,4 +1,4 @@
-{...}: {
+{pkgs, ...}: {
   imports = [
     ./hardware-configuration.nix
   ];
@@ -7,4 +7,22 @@
 
   # Configure console keymap
   console.keyMap = "br-abnt2";
+
+  # GPU
+  nixpkgs.config.packageOverrides = pkgs: {
+    intel-vaapi-driver = pkgs.intel-vaapi-driver.override {enableHybridCodec = true;};
+  };
+
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      libvdpau-va-gl
+    ];
+  };
+
+  environment.sessionVariables = {LIBVA_DRIVER_NAME = "iHD";}; # Force intel-media-driver
 }
