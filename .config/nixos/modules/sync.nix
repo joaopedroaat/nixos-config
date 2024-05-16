328f01c2e4faf2c1861351140c05ff780c3d1350
joaopedroaat <joaopedroaat@outlook.com>
2024-05-15 21:46:54 -0300

    Add more folders

--- .config/nixos/modules/sync.nix
+++ .config/nixos/modules/sync.nix
@@ -14,6 +14,54 @@
 
       overrideDevices = true;
       overrideFolders = true;
+
+      settings = let
+        hostname = config.networking.hostName;
+      in {
+        devices = {
+          banana-tree = lib.mkIf (hostname == "banana-tree") {
+            id = "BDUIVNQ-IWB4RYX-LXMCIMB-GWBDLCW-ONAFMCN-MCWV5LP-QCCNQUA-2K6AEQD";
+          };
+          banana-leaf = lib.mkIf (hostname == "banana-tree") {
+            id = "7BHQOOK-QQSS2AG-ZOOHCGZ-J7EF4W3-KJHTWXS-CBWA7P7-E52CPT4-33TNYA2";
+          };
+        };
+
+        folders = let
+          devices = [
+            (
+              if hostname == "banana-tree"
+              then "banana-leaf"
+              else "banana-tree"
+            )
+          ];
+        in {
+          "Documents" = {
+            path = "/home/joaopedroaat/Documents";
+            inherit devices;
+          };
+
+          "Pictures" = {
+            path = "/home/joaopedroaat/Pictures";
+            inherit devices;
+          };
+
+          "Projects" = {
+            path = "/home/joaopedroaat/Projects";
+            inherit devices;
+          };
+
+          "Music" = {
+            path = "/home/joaopedroaat/Music";
+            inherit devices;
+          };
+
+          "Videos" = {
+            path = "/home/joaopedroaat/Music";
+            inherit devices;
+          };
+        };
+      };
     };
 
     networking.firewall = {
