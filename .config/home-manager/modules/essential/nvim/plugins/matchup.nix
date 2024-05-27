d1579856a687bef1f32ef8e90347f4a5aa1cef40
joaopedroaat <joaopedroaat@outlook.com>
2024-05-26 22:13:57 -0300

    Add matchip plugin

---
+++ .config/home-manager/modules/essential/nvim/plugins/matchup.nix
@@ -0,0 +1,29 @@
+{...}: {
+  programs.nixvim = {
+    plugins.vim-matchup = {
+      enable = true;
+    };
+
+    keymaps = [
+      {
+        key = "<leader>m";
+        lua = true;
+        action =
+          /*
+          lua
+          */
+          ''
+            function()
+              local enabled = vim.g.matchup_enabled
+              if enabled == 0 then
+                  vim.g.matchup_enabled = 1
+                else
+                  vim.g.matchup_enabled = 0
+              end
+            end
+          '';
+        options = {desc = "Toggle matchup";};
+      }
+    ];
+  };
+}
