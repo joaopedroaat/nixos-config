e6e7f94190e193d8fdd56f799bc021e5c70656df
joaopedroaat <joaopedroaat@outlook.com>
2024-06-19 14:19:47 -0300

    Add fzf integration

--- .config/home-manager/modules/terminal-tools/fzf.nix
+++ .config/home-manager/modules/terminal-tools/fzf.nix
@@ -1,31 +1,13 @@
 {
   lib,
   config,
-  pkgs,
   ...
 }: {
   options.fzf.enable = lib.mkEnableOption "Terminal tools";
   config = lib.mkIf config.fzf.enable {
-    home.packages = with pkgs; [
-      fzf
-    ];
-
-    programs.fish = {
-      functions = {
-        fish_user_key_bindings = {
-          body =
-            /*
-            fish
-            */
-            ''
-              if command -s fzf-share >/dev/null
-                source (fzf-share)/key-bindings.fish
-              end
-
-              fzf_key_bindings
-            '';
-        };
-      };
+    programs.fzf = {
+      enable = true;
+      enableZshIntegration = true;
     };
   };
 }
