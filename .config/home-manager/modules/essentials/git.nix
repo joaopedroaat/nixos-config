2e6d6867373e3ce1d6b9a95da705f6dcbd8bccf1
joaopedroaat <joaopedroaat@outlook.com>
2024-06-19 23:59:33 -0300

    Add github cli

--- .config/home-manager/modules/essentials/git.nix
+++ .config/home-manager/modules/essentials/git.nix
@@ -5,21 +5,25 @@
 }: {
   options.git.enable = lib.mkEnableOption "Git";
   config = lib.mkIf config.git.enable {
-    programs.git = {
-      enable = true;
-      userName = "joaopedroaat";
-      userEmail = "joaopedroaat@outlook.com";
-      extraConfig = {
-        init.defaultBranch = "main";
-      };
-    };
-
-    programs.lazygit = {
-      enable = true;
-      settings = {
-        git = {
-          autoFetch = false;
+    programs = {
+      git = {
+        enable = true;
+        userName = "joaopedroaat";
+        userEmail = "joaopedroaat@outlook.com";
+        extraConfig = {
+          init.defaultBranch = "main";
         };
+      };
+      lazygit = {
+        enable = true;
+        settings = {
+          git = {
+            autoFetch = false;
+          };
+        };
+      };
+      gh = {
+        enable = true;
       };
     };
   };
