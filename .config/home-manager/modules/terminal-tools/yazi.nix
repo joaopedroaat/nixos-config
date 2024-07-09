c7cd40644041234236146e96d5d048f52665209f
joaopedroaat <joaopedroaat@outlook.com>
2024-07-09 20:56:41 -0300

    Config yazi keymaps

--- .config/home-manager/modules/terminal-tools/yazi.nix
+++ .config/home-manager/modules/terminal-tools/yazi.nix
@@ -9,6 +9,37 @@
     programs.yazi = {
       enable = true;
       enableZshIntegration = true;
+      settings = {
+        log = {
+          enabled = true;
+        };
+      };
+      keymap = {
+        manager.prepend_keymap = [
+          {
+            on = ["<C-n>"];
+            run = ''
+              shell '${pkgs.xdragon}/bin/xdragon -x -i -T "$@"' --confirm
+            '';
+            desc = "Drag select files with Dragon";
+          }
+          {
+            on = ["y"];
+            run = [
+              "yank"
+              ''
+                shell --confirm 'for path in "$@"; do echo "file://$path"; done | ${pkgs.wl-clipboard}/bin/wl-copy -t text/uri-list'
+              ''
+            ];
+            desc = "Yank to clipboard";
+          }
+          {
+            on = ["<C-s>"];
+            run = "shell '$SHELL' --block --confirm";
+            desc = "Open shell here";
+          }
+        ];
+      };
     };
 
     home.packages = with pkgs; [
