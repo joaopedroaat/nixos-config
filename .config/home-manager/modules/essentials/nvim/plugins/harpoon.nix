975e8493718d0ca7516d3a644fce094ff76d6330
joaopedroaat <joaopedroaat@outlook.com>
2024-07-19 22:56:45 -0300

    Add which key desc to harpoon keymaps

--- .config/home-manager/modules/essentials/nvim/plugins/harpoon.nix
+++ .config/home-manager/modules/essentials/nvim/plugins/harpoon.nix
@@ -14,20 +14,37 @@
 
       # Each git branch has it's own marks
       markBranch = true;
-
-      keymaps = {
-        addFile = "ma";
-        navPrev = "mp";
-        navNext = "mn";
-        toggleQuickMenu = "mq";
-      };
     };
 
     keymaps = [
       {
         action = "<cmd>Telescope harpoon marks<CR>";
         key = "<leader>fm";
-        options = {desc = "Find marks";};
+        options = {desc = "Find marks (Telescope)";};
+      }
+      {
+        mode = "n";
+        action = "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>";
+        key = "mq";
+        options = {desc = "Find marks (Harpoon)";};
+      }
+      {
+        mode = "n";
+        action = "<cmd>lua require('harpoon.mark').add_file()<CR>";
+        key = "ma";
+        options = {desc = "Mark file";};
+      }
+      {
+        mode = "n";
+        action = "<cmd>lua require('harpoon.ui').nav_next()<CR>";
+        key = "mn";
+        options = {desc = "Next mark";};
+      }
+      {
+        mode = "n";
+        action = "<cmd>lua require('harpoon.ui').nav_prev()<CR>";
+        key = "mp";
+        options = {desc = "Prev mark";};
       }
     ];
   };
